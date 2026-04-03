c0e1515023ad3fc3184e3e02de0ed56d3b802596
Simon <simon@fantom.foundation>
2026-04-03 09:52:29 +0200

    tests, cmd/evm: make test infrastructure configurable for external use
    - Make ethereum tests configurable to inject DB implementations
    - Make testmatcher accessible from outside test package

--- eth/tracers/internal/tracetest/calltrace_test.go
+++ eth/tracers/internal/tracetest/calltrace_test.go
@@ -124,7 +124,7 @@ func testCallTracer(tracerName string, dirPath string, t *testing.T) {
 			}
 			logState := vm.StateDB(st.StateDB)
 			if tracer.Hooks != nil {
-				logState = state.NewHookedState(st.StateDB, tracer.Hooks)
+				logState = state.NewHookedState(st.StateDB.(*state.StateDB), tracer.Hooks)
 			}
 			msg, err := core.TransactionToMessage(tx, signer, context.BaseFee)
 			if err != nil {
@@ -356,7 +356,7 @@ func TestInternals(t *testing.T) {
 
 			logState := vm.StateDB(st.StateDB)
 			if hooks := tc.tracer.Hooks; hooks != nil {
-				logState = state.NewHookedState(st.StateDB, hooks)
+				logState = state.NewHookedState(st.StateDB.(*state.StateDB), hooks)
 			}
 
 			tx, err := types.SignNewTx(key, signer, &types.LegacyTx{
